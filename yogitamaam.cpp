710cc9f5ff8db50100c669bc1b2b089e736868f5
Atulpanwar <atulpanwar2001@gmail.com>
2022-01-27 15:07:11 +0530

    maam

---
+++ yogitamaam.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int  arr[10];
+     arr[0]=1;
+    arr[1]=1;
+     cout<<arr[0]<<" "<<arr[1]<<" ";
+    for(int i=2;i<10;i++)
+    {
+        arr[i]= arr[i-1]+arr[i-2];
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
